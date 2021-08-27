d8e88a998eaaa4352eb4839c7fccb112e65946aa
toan94 <toanbui0517@gmail.com>
2021-08-27 15:02:18 +0700

    done 2/3

--- src/main/java/com/toan/project/controllers/PlaylistController.java
+++ src/main/java/com/toan/project/controllers/PlaylistController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -57,14 +58,14 @@ public class PlaylistController {
                 return new ResponseEntity<>(response, HttpStatus.OK);
             }
             List<Playlist> playlists = new ArrayList<>();
-            Pageable paging = PageRequest.of(page, size);
+            Pageable paging = PageRequest.of(page, size, Sort.by("id").descending());
 
             Page<Playlist> pagePlaylists;
-//            if (title == null)
-//                pagePlaylists = playlistRepository.findAll(paging);
-//            else
-//                pagePlaylists = playlistRepository.findByNameContaining(title, paging);
-            pagePlaylists = playlistRepository.findAll(paging);
+            if (title == null)
+                pagePlaylists = playlistRepository.findAll(paging);
+            else
+                pagePlaylists = playlistRepository.findByNameContaining(title, paging);
+//            pagePlaylists = playlistRepository.findAll(paging);
 
             playlists = pagePlaylists.getContent();
             List<PlaylistPayload> pPayload = playlists.stream().map((p)->{
--- src/main/java/com/toan/project/repository/PlaylistRepository.java
+++ src/main/java/com/toan/project/repository/PlaylistRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
     Page<Playlist> findByNameContaining(String title, Pageable pageable);
-
+    List<Playlist> findAllByOrderByCreationDateAsc();
 }
